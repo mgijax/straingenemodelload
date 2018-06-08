c0cdb74bf8567ef1ff66eb5c8fd1ae9754135410
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2018-06-08 07:21:33 -0400

    Added MV annotation load

---
+++ bin/createMCVAnnots.py
@@ -0,0 +1,271 @@
+#!/usr/local/bin/python
+
+##########################################################################
+#
+# Purpose:
+#       creates Strain/Marker annotation load input file
+#
+Usage='createMCVAnnots.py'
+#
+# Env Vars:
+# Inputs: 
+#	1. mgd database 
+#
+# Outputs:
+#	 1. annotload input files
+# 	 2. Log file
+#
+# Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#
+#  Assumes:  Nothing
+#
+#  Notes:  None
+#
+###########################################################################
+#
+#  Modification History:
+#
+#  Date        SE   Change Description
+#  ----------  ---  -------------------------------------------------------
+#
+#  06/01/2018  sc   Initial development
+#
+###########################################################################
+
+import sys
+import os
+import string
+import mgi_utils
+import db
+
+#
+# CONSTANTS 
+#
+TAB= '\t'
+CRT = '\n'
+
+# MGI_User key for the load
+CREATEDBY_KEY = 1600
+
+B6_ONLY = os.environ['B6_ONLY']
+
+# JNumbers for MGI B6 and MGP annotations, we want to be able to 
+# delete by reference used in columns 3 (reference) and 5 (inferred from)
+B6_JNUM = 'J:260092'
+MGP_JNUM = 'J:259852'
+
+# evidence code
+EVIDENCE = 'IC'
+
+# null qualifier
+QUALIFIER = ''
+
+# Editor, MGI_User.login
+EDITOR = 'cjb'
+
+# current date use null
+DATE = ''
+
+# no notes
+NOTES = ''
+
+# Logical DB
+LDBNAME = 'MGI Strain Gene'
+
+
+#
+# GLOBALS
+#
+
+# annotation load file and file descriptor
+b6AnnotFile = os.environ['MCV_B6_ANNOTLOAD_INPUTFILE']
+fpB6AnnotFile = ''
+
+mgpAnnotFile = os.environ['MCV_MGP_ANNOTLOAD_INPUTFILE']
+fpMgpAnnotFile = ''
+
+#
+# lookups
+#
+featureTypeB6Lookup = {}
+featureTypeMGPLookup = {}
+
+# Purpose:  Load mcv lookup for B6
+# Returns: nothing
+# Assumes: there is a connection to the database
+# Effects: nothing
+# Throws: nothing
+
+def loadFeatureTypeB6Lookup():
+    global featureTypeB6Lookup
+    results = db.sql('''select t.term, a.accid as mcvID
+	from VOC_Term t, ACC_Accession a
+	where t._Vocab_key = 79 --MCV
+	and t._Term_key = a._Object_key
+	and a._MGIType_key = 13
+	and a._LogicalDB_key = 146 --MCV''', 'auto')
+    for r in results:
+	featureTypeB6Lookup[r['term']] = r['mcvID']
+
+    return 0
+
+# Purpose: load rawBiotype Lookup for MGP 
+# Returns: nothing
+# Assumes: there is a connection to the database
+# Effects: nothing
+# Throws: nothing
+
+def loadFeatureTypeMGPLookup():
+    global featureTypeMGPLookup
+
+    # load the MGP biotype translation into a lookup
+    results = db.sql('''select distinct sgm.rawBiotype, a.accid as mcvID
+        from SEQ_GeneModel sgm, VOC_Term t, MRK_BiotypeMapping m, ACC_Accession a
+        where sgm.rawBiotype = t.term
+        and t._Vocab_key = 136 --Biotype MGP
+        and t._Term_key = m._BiotypeTerm_key
+        and m._mcvTerm_key = a._Object_key
+        and a._MGIType_key = 13
+        and a._LogicalDB_key = 146 --MCV''', 'auto')
+    for r in results:
+        featureTypeMGPLookup[r['rawBiotype']] = r['mcvID']
+    return 0
+
+# Purpose: init file descriptors, load lookups 
+# Returns: nothing
+# Assumes: nothing
+# Effects: nothing
+# Throws: nothing
+
+def init():
+    global fpB6AnnotFile, fpMgpAnnotFile
+    global loadFeatureTypeB6Lookup, loadFeatureTypeMGPLookup
+
+    try:
+        fpB6AnnotFile = open(b6AnnotFile, 'w')
+    except:
+        'Could not open file for writing %s\n' % b6AnnotFile
+        sys.exit(1)
+
+    try:
+        fpMgpAnnotFile = open(mgpAnnotFile, 'w')
+    except:
+        'Could not open file for writing %s\n' % mgpAnnotFile
+        sys.exit(1)
+
+    loadFeatureTypeB6Lookup()
+    loadFeatureTypeMGPLookup()
+
+    # Log all SQL
+    db.set_sqlLogFunction(db.sqlLogAll)
+    db.useOneConnection(1)
+
+    return 0
+
+# Purpose: processes B6 writes to bcp file
+# Returns: nothing
+# Assumes: nothing
+# Effects: creates file in the filesystem
+# Throws: nothing
+def processB6():
+    print 'Processing B6'
+    db.sql('''-- get b6 strain/markers and their accids
+	select a.accid as b6ID, a._Object_key as _StrainMarker_key
+	into temporary table b6Ids
+	from ACC_Accession a
+	where a._MGIType_key = 44 --MRK_StrainMarker
+	and a._LogicalDB_key = 212 --MGI B6
+	and a.preferred = 1''', None)
+
+    db.sql('''create index idx1 on b6Ids(b6ID)''', None)
+    
+    results = db.sql('''-- get the raw biotypes for the B6 gene models
+	select b6.b6ID, sgm.rawBiotype
+	from b6Ids b6, ACC_Accession a, SEQ_GeneModel sgm
+	where b6.b6ID = a.accid
+	and a._MGIType_key = 19
+	and a._LogicalDB_key = 212 --MGI B6
+	and a.preferred = 1
+	and a._Object_key = sgm._Sequence_key''', 'auto')
+    
+    for r in results:
+	b6ID = r['b6ID']
+	rawBiotype = r['rawBiotype'] # actually the feature type
+	if rawBiotype not in featureTypeB6Lookup:
+	    print 'Cannot resolve B6: %s to Feature Type' % rawBiotype
+	    continue
+	mcvID = featureTypeB6Lookup[rawBiotype]
+        fpB6AnnotFile.write('%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s' % (mcvID, TAB, b6ID, TAB, B6_JNUM, TAB, EVIDENCE, TAB, B6_JNUM, TAB, QUALIFIER, TAB, EDITOR, TAB, DATE, TAB, NOTES, TAB, LDBNAME, CRT))
+
+    return 0
+
+# Purpose: processes MGP writes to bcp file
+# Returns: nothing
+# Assumes: nothing
+# Effects: creates file in the filesystem
+# Throws: nothing
+def processMGP():
+    print 'Processing MGP'
+    db.sql('''-- get mgp strain/markers and their accids
+	select a.accid as mgpID, a._Object_key as _StrainMarker_key
+	into temporary table mgpIds
+	from ACC_Accession a
+	where a._MGIType_key = 44 --MRK_StrainMarker
+	and a._LogicalDB_key = 209 --MGP
+	and a.preferred = 1''', None)
+    db.sql('''create index idx2 on mgpIds(mgpID)''', None)
+
+    results = db.sql('''-- get the raw biotypes for the MGP gene models
+	select mgp.mgpID, sgm.rawBiotype
+	from mgpIds mgp, ACC_Accession a, SEQ_GeneModel sgm
+	where mgp.mgpID = a.accid
+	and a._MGIType_key = 19
+	and a._LogicalDB_key = 209 --MGP
+	and a.preferred = 1
+	and a._Object_key = sgm._Sequence_key''', 'auto')
+    
+    for r in results:
+        mgpID = r['mgpID']
+        rawBiotype = r['rawBiotype'] 
+        if rawBiotype not in featureTypeMGPLookup:
+            print 'Cannot resolve MGP: %s to Feature Type' % rawBiotype
+            continue
+        mcvID = featureTypeMGPLookup[rawBiotype]
+        fpMgpAnnotFile.write('%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s' % (mcvID, TAB, mgpID, TAB, MGP_JNUM, TAB, EVIDENCE, TAB, MGP_JNUM, TAB, QUALIFIER, TAB, EDITOR, TAB, DATE, TAB, NOTES, TAB, LDBNAME, CRT))
+
+    return 0
+
+
+# Purpose: creates the bcp file
+# Returns: nothing
+# Assumes: nothing
+# Effects: creates file in the filesystem
+# Throws: nothing
+
+def run ():
+    if B6_ONLY == 'false':
+	processMGP()
+
+    processB6()
+
+    return 0
+#
+# Main
+#
+
+print '%s' % mgi_utils.date()
+print 'Initializing'
+init()
+
+print '%s' % mgi_utils.date()
+run()
+
+fpB6AnnotFile.close()
+fpMgpAnnotFile.close()
+
+db.useOneConnection(0)
+
+print '%s' % mgi_utils.date()
---
+++ bin/createMCVAnnots.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+#
+#  createMCVAnnots.sh
+###########################################################################
+#
+#  Purpose:
+#
+#      This script calls createMCVAnnots.py to create MCV annotload file
+#	Then invokes annotload to load MCV annotations to strain/markers
+#
+#  Usage:
+#
+#      createMCVAnnots.sh
+#
+#
+#  Env Vars:
+#
+#
+#  Inputs: the database and configuration
+#
+#  Outputs:
+#       - annotation load input file
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#
+#  Assumes:  Nothing
+#
+#  Implementation:
+#
+#      This script will perform following steps:
+#
+#      1) Call the python script (createMCVAnnots.py) input file for
+#	     strain/marker MCV annotations (delete and reload)
+#      2) Call the annotload to load the annotations
+#
+#  Notes:  None
+#
+###########################################################################
+#
+#  Modification History:
+#
+#  Date        SE   Change Description
+#  ----------  ---  -------------------------------------------------------
+#
+#  06/01/2018  sc   TR12734 genfevah project
+#
+###########################################################################
+
+cd `dirname $0`
+
+CONFIG=straingenemodelload.config
+#
+# Make sure the common configuration file exists and source it.
+#
+if [ -f ../${CONFIG} ]
+then
+    . ../${CONFIG}
+else
+    echo "Missing configuration file: ${CONFIG}"
+    exit 1
+fi
+
+B6_CONFIG=${STRAINGENEMODELLOAD}/b6_annotload.csh.config
+MGP_CONFIG=${STRAINGENEMODELLOAD}/mgp_annotload.csh.config
+
+#
+# Initialize the log file.
+#
+LOG=${MCV_LOGFILE}
+
+if [ -f ${LOG} ]
+then
+    rm ${LOG}
+fi
+
+echo 'Creating MCV annotations' >> ${LOG}
+date >> ${LOG}
+
+echo "Creating MCV annotation load input file" | tee -a  ${LOG}
+./createMCVAnnots.py >> ${LOG} 2>&1
+STAT=$?
+if [ $STAT -ne 0 ]
+then
+   echo "createMCVAnnots.py failed" | tee -a ${LOG}
+   exit 1
+fi
+
+#  Call the annotation loader
+#
+echo "" >> ${LOG}
+date >> ${LOG}
+
+if [ "${B6_ONLY}" = "false" ]
+then
+    echo 'Loading MGP MCV Annotations'
+    ${ANNOTLOAD}/annotload.csh ${MGP_CONFIG} mcv
+fi
+
+echo 'Loading B6 MCV Annotations'
+${ANNOTLOAD}/annotload.csh ${B6_CONFIG} mcv
+
+date >> ${LOG}
+
+exit 0
